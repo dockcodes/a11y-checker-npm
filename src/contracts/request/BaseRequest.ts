3651d5664fb9ffe2711200fa754b1d4f6530d908
milosz <milosz.miotk@studio-creativa.pl>
2025-11-04 11:51:21 +0100

    added recaprcha token to scan request type and made user key nullable

--- src/contracts/request/BaseRequest.ts
+++ src/contracts/request/BaseRequest.ts
@@ -1,3 +1,3 @@
 export interface BaseRequest {
-    key?: string;
+    key?: string | null;
 }
--- src/contracts/request/ScanRequest.ts
+++ src/contracts/request/ScanRequest.ts
@@ -2,6 +2,7 @@ import type { Device, Language } from '../types';
 import type { BaseRequest } from './BaseRequest';
 
 export interface ScanRequest extends BaseRequest {
+  recaptchaToken: string;
   url: string;
   lang?: Language;
   device?: Device;
