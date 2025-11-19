ee319974383127b70f014218a5b4246df0fac769
milosz <milosz.miotk@studio-creativa.pl>
2025-11-19 18:57:43 +0100

    updated audit response to include inprogress sta

--- src/contracts/response/AuditResponse.ts
+++ src/contracts/response/AuditResponse.ts
@@ -1,24 +1,35 @@
-import { AuditContent } from "../types";
-import { BaseResponse } from "./BaseResponse";
+import { AuditContent } from '../types';
+import { BaseResponse } from './BaseResponse';
 
 interface AuditBase {
-  address_uuid: string;
-  created_at: string;
-  expires_at: string | null;
-  requests_left: number;
-  updated_at: string;
-  url: string;
-  uuid: string;
+    address_uuid: string;
+    created_at: string;
+    expires_at: string | null;
+    requests_left: number;
+    updated_at: string;
+    url: string;
+    uuid: string;
 }
 export interface AuditSuccess extends AuditBase {
-  desktop?: AuditContent;
-  mobile?: AuditContent;
-  title: string;
-  status: 'SUCCESS' | 'QUEUED';
+    desktop?: AuditContent;
+    mobile?: AuditContent;
+    title: string;
+    status: 'SUCCESS';
+}
+
+export interface AuditQueued extends AuditBase {
+    status: 'QUEUED';
+    title: string;
+}
+export interface AuditInProgress extends AuditBase {
+    status: 'IN_PROGRESS';
+    desktop?: Pick<AuditContent, 'screenshot'>;
+    mobile?: Pick<AuditContent, 'screenshot'>;
+    title: string;
 }
 
 export interface AuditFailed extends AuditBase {
-  status: 'FAILED';
+    status: 'FAILED';
 }
 
 export type AuditResponse = BaseResponse<AuditSuccess | AuditFailed>;
